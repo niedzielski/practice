47aa34f276acb94eee56dce28f2120efef5bf0e4
Stephen Niedzielski <stephen@niedzielski.com>
2021-11-25 21:04:17 -0700

    Add heap

---
+++ src/heap.test.ts
@@ -0,0 +1,43 @@
+import * as Heap from './heap'
+
+it('insert: ascending sorted insertion', () => {
+  const heap = Heap.make<number>((left, right) => left - right)
+  Heap.insert(heap, 1, 2, 3, 4, 5)
+  const ret = []
+  while (!Heap.isEmpty(heap)) ret.push(Heap.remove(heap))
+  expect(ret).toStrictEqual([1, 2, 3, 4, 5])
+})
+
+it('insert: descending sorted insertion', () => {
+  const heap = Heap.make<number>((left, right) => left - right)
+  Heap.insert(heap, 5, 4, 3, 2, 1)
+  const ret = []
+  while (!Heap.isEmpty(heap)) ret.push(Heap.remove(heap))
+  expect(ret).toStrictEqual([1, 2, 3, 4, 5])
+})
+
+it('insert: diagram', () => {
+  const heap = Heap.make<number>((left, right) => left - right)
+  const vals = [
+    21, 45, 43, 46, 51, 56, 50, 60, 70, 62, 52, 57, 58, 53, 54, 61, 63, 88
+  ]
+  Heap.insert(heap, ...vals)
+  expect(heap.nodes).toStrictEqual(vals)
+})
+
+it('remove: diagram', () => {
+  const vals = [
+    21, 45, 43, 46, 51, 56, 50, 60, 70, 62, 52, 57, 58, 53, 54, 61, 63, 88
+  ]
+  const heap = Heap.make<number>((left, right) => left - right)
+  Heap.insert(heap, ...vals)
+  const ret = []
+  while (!Heap.isEmpty(heap)) ret.push(Heap.remove(heap))
+  expect(Heap.getSize(heap)).toStrictEqual(0)
+  expect(ret).toStrictEqual([...vals].sort())
+})
+
+it('remove: underflow', () => {
+  const heap = Heap.make<number>((left, right) => left - right)
+  expect(() => Heap.remove(heap)).toThrow()
+})
---
+++ src/heap.ts
@@ -0,0 +1,112 @@
+// The heap is an array of nodes where:
+// - Children are always greater than or equal to the parent.
+// - Children are stored leftmost in the tree. (The tree is balanced and
+//   complete but not necessarily perfectly.) This is so that nodes are laid out
+//   predictably.
+//   - The head is the first node (zeroth index).
+//   - The parent of any node is ~~((index + 1) / 2) - 1. -1 indicates no
+//     parent.
+//   - The children of any node, when present, are adjacent at
+//     (index + 1) * 2 - 1 and (index + 1) * 2.
+// Ex:
+//                ___________21___________
+//               /                        \
+//         _____45_____               ____43____
+//        /            \             /          \
+//     __46__        __51__       __56__      __50__
+//    /      \      /      \     /      \    /      \
+//   60      70    62      52   57      58  53      54
+//  /  \    /
+// 61  63  88
+// [21, 45, 43, 46, 51, 56, 50, 60, 70, 62, 52, 57, 58, 53, 54, 61, 63, 88]
+//  0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17
+// Example hierarchy:
+// - The parent of 88 is at index = ~~((17 + 1) / 2) - 1 = ~~(18 / 2) - 1 = 8
+// - The parent of 70 is at index = ~~(( 8 + 1) / 2) - 1 = ~~( 9 / 2) - 1 = 3
+// - The parent of 46 is at index = ~~(( 3 + 1) / 2) - 1 = ~~( 4 / 2) - 1 = 1
+// - The parent of 45 is at index = ~~(( 1 + 1) / 2) - 1 = ~~( 2 / 2) - 1 = 0
+// - The parent of 21 is at index = ~~(( 0 + 1) / 2) - 1 = ~~( 1 / 2) - 1 = -1
+
+export type Heap<T> = {nodes: T[]; compare: Compare<T>}
+
+/**
+ * @return Negative when lhs < rhs, zero when lhs == rhs, and positive when
+ *         lhs > rhs.
+ */
+export type Compare<T> = (lhs: T, rhs: T) => number
+
+export function make<T>(compare: Compare<T>): Heap<T> {
+  return {nodes: [], compare}
+}
+
+export function remove<T>({nodes, compare}: Heap<T>): T {
+  // The top is the minimum.
+  const min = nodes[0]
+
+  if (min == null) throw Error('Heap underflow.')
+
+  // Move the last node to the top unless it is the last.
+  const end = nodes.pop()!
+  if (nodes.length > 0) nodes[0] = end
+
+  // Bubble down from the very top.
+  for (let i = 0; i < nodes.length; ) {
+    const leftIndex = 2 * i + 1 //(i + 1) * 2 - 1 // = 2 * i + 2 -1 = 2i + 1
+    const rightIndex = leftIndex + 1 //(i + 1) * 2
+
+    if (
+      leftIndex < nodes.length &&
+      compare(nodes[leftIndex]!, nodes[i]!) < 0 &&
+      (rightIndex >= nodes.length ||
+        compare(nodes[leftIndex]!, nodes[rightIndex]!) <= 0)
+    ) {
+      swap(nodes, i, leftIndex)
+      i = leftIndex
+    } else if (
+      rightIndex < nodes.length &&
+      compare(nodes[rightIndex]!, nodes[i]!) < 0
+    ) {
+      swap(nodes, i, rightIndex)
+      i = rightIndex
+    } else break
+  }
+
+  return min
+}
+
+export function insert<T>(heap: Heap<T>, ...nodes: T[]): void {
+  nodes.forEach(node => insertOne(heap, node))
+}
+
+export function getSize<T>(heap: Heap<T>): number {
+  return heap.nodes.length
+}
+
+export function isEmpty<T>(heap: Heap<T>): boolean {
+  return heap.nodes.length == 0
+}
+
+function insertOne<T>({nodes, compare}: Heap<T>, node: T): void {
+  // Add to the end (bottom left of the tree).
+  nodes[nodes.length] = node
+
+  // Bubble up from the very bottom while a parent exists (index > 0) and less
+  // than parent.
+  for (
+    let i = nodes.length - 1;
+    i > 0 && compare(nodes[i]!, nodes[parentIndex(i)]!) < 0;
+    i = parentIndex(i)
+  )
+    swap(nodes, i, parentIndex(i))
+}
+
+/**
+ * @return The index of the parent of the node at index. -1 indicates no parent.
+ */
+function parentIndex(index: number): number {
+  return Math.trunc((index - 1) / 2)
+}
+
+function swap<T>(nodes: T[], left: number, right: number): void {
+  ;[nodes[left], nodes[right]] = [nodes[right]!, nodes[left]!]
+}
