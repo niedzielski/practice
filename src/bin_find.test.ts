e11a648359d1f93bf1665271ce8e658945496909
Stephen Niedzielski <stephen@niedzielski.com>
2021-11-25 19:05:56 -0700

    Add binary searches

---
+++ src/bin_find.test.ts
@@ -0,0 +1,21 @@
+import {bin_find, bin_find_recursive} from './bin_find'
+
+it.each([
+  ['empty', [], 1, undefined],
+  ['one found', [1], 1, 0],
+  ['one not found', [1], 2, undefined],
+  ['two found', [1, 2], 2, 1],
+  ['two not found', [1, 3], 2, undefined],
+  ['three found', [1, 2, 3], 2, 1],
+  ['three not found', [1, 2, 3], 0, undefined],
+  ['four found', [1, 2, 3, 4], 1, 0],
+  ['four not found', [1, 2, 3, 5], 4, undefined],
+  ['five found', [1, 2, 3, 4, 5], 3, 2],
+  ['five found 2', [1, 2, 3, 4, 5], 5, 4],
+  ['five not found', [1, 2, 3, 4, 5], 6, undefined]
+])('%s', (_, nums, num, expected) => {
+  expect(bin_find(nums, num)).toStrictEqual(expected)
+  expect(bin_find_recursive(nums, num, (lhs, rhs) => lhs - rhs)).toStrictEqual(
+    expected
+  )
+})
---
+++ src/bin_find.ts
@@ -0,0 +1,40 @@
+export function bin_find(
+  nums: readonly number[],
+  num: number
+): number | undefined {
+  let min = 0 // inclusive
+  let max = nums.length // exclusive
+  while (min < max) {
+    const mid = min + Math.trunc((max - min) / 2)
+    if (nums[mid] == num) return mid
+    if (nums[mid]! < num) min = mid + 1
+    else max = mid
+  }
+}
+
+/**
+ * @arg min Inclusive minimum.
+ * @arg max Exclusive maximum.
+ * @return Index if present.
+ */
+export function bin_find_recursive<T>(
+  items: T[],
+  item: T,
+  compare: (left: T, right: T) => number,
+  min: number = 0,
+  max: number = items.length
+): number | undefined {
+  if (min >= max) return
+
+  const mid = Math.trunc((max + min) / 2)
+  const comparison = compare(item, items[mid]!)
+  if (comparison == 0) return mid
+
+  // If item is less than middle, item can only be in the left. Search the left.
+  // middle has been checked and so is correct for exclusive max.
+  if (comparison < 0) return bin_find_recursive(items, item, compare, min, mid)
+
+  // If item is greater than middle, item can only be in the right. Search the
+  // right. min is inclusive so add one to skip middle which has been checked.
+  return bin_find_recursive(items, item, compare, mid + 1, max)
+}
