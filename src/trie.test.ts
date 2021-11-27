aec0468c76153c11df078fb65573b21b4727f300
Stephen Niedzielski <stephen@niedzielski.com>
2021-11-26 21:03:20 -0700

    Add trie

---
+++ src/trie.test.ts
@@ -0,0 +1,36 @@
+import * as Trie from './trie'
+
+it('Empty completion', () => {
+  const trie = Trie.make()
+  expect(Trie.complete(trie, 'foo', 10)).toStrictEqual([])
+})
+
+it('Complete small words', () => {
+  const trie = Trie.make()
+
+  Trie.add(trie, 'a')
+  expect(Trie.complete(trie, 'a', 10)).toStrictEqual(['a'])
+
+  Trie.add(trie, 'oh')
+  expect(Trie.complete(trie, 'o', 10)).toStrictEqual(['oh'])
+
+  Trie.add(trie, 'it')
+  expect(Trie.complete(trie, 'i', 10)).toStrictEqual(['it'])
+
+  Trie.add(trie, 'ok')
+  expect(Trie.complete(trie, 'o', 10)).toStrictEqual(['oh', 'ok'])
+})
+
+it('New', () => {
+  const trie = Trie.make()
+  Trie.add(trie, 'New Jersey')
+  Trie.add(trie, 'Pennsylvania')
+  Trie.add(trie, 'New Mexico')
+  Trie.add(trie, 'Ohio')
+  Trie.add(trie, 'Washington')
+  expect(Trie.complete(trie, 'New', 10)).toStrictEqual([
+    'New Jersey',
+    'New Mexico'
+  ])
+  expect(Trie.complete(trie, 'New', 1)).toStrictEqual(['New Jersey'])
+})
---
+++ src/trie.ts
@@ -0,0 +1,47 @@
+export type Node = {prefix: string; word?: boolean; children: Node[]}
+
+export function make(): Node {
+  return {prefix: '', children: []}
+}
+
+export function add(trie: Node, word: string): void {
+  addFrom(nearest(trie, word), word)
+}
+
+function nearest(trie: Node, word: string): Node {
+  let node = trie
+  let prefix = ''
+  for (const letter of word) {
+    prefix += letter
+    const child = node.children.find(node => node.prefix === prefix)
+    if (child != null) node = child
+    else break
+  }
+  return node
+}
+
+function addFrom(node: Node, word: string): void {
+  let {prefix} = node
+  const remaining = word.slice(prefix.length)
+  for (const letter of remaining) {
+    prefix += letter
+    const child = {prefix, children: []}
+    node.children.push(child)
+    node = child
+  }
+  node.word = true
+}
+
+export function complete(trie: Node, word: string, limit: number): string[] {
+  let matches = []
+  let queue = [nearest(trie, word)]
+  for (
+    let node = queue.shift();
+    node != null && matches.length < limit;
+    node = queue.shift()
+  ) {
+    if (node.word) matches.push(node.prefix)
+    queue.push(...node.children)
+  }
+  return matches
+}
